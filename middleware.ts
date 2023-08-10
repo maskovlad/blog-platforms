741dbec94146976d9fd74750f7f490e5a38c89da
maskovlad <maskovladnik@gmail.com>
2023-08-10 11:52:14 +0300

    Del console logs

--- middleware.ts
+++ middleware.ts
@@ -7,9 +7,8 @@ export const config = {
      - Match all paths except for:
      - 1. /api routes
      - 2. /_next (Next.js internals)
-     - 3. /fonts (inside /public)
-     - 4. /examples (inside /public)
-     - 5. all root files inside /public (e.g. /favicon.ico)
+     - 3. /_static (inside /public)
+     - 4. all root files inside /public (e.g. /favicon.ico)
      */
     "/((?!api/|_next/|_static/|_vercel|[\\w-]+\\.\\w+).*)",
   ],
@@ -22,22 +21,17 @@ export default async function middleware(req: NextRequest) {
 
   // Get the pathname of the request (e.g. /, /about, /blog/first-post)
   const path = url.pathname;
-  console.log({path})
-
-  const session = await getToken({ req });
-  console.log({session})
 
   const currentHost =
     process.env.NODE_ENV === "production" && process.env.VERCEL === "1"
       ? hostname
         .replace('.sviy.site', "")
       : hostname.replace(/\.localhost:\d+/, "");
-  //  console.log({currentHost})
 
    // rewrites for app pages
   if (currentHost == "app") {
     if (
-      url.pathname === "/login" &&
+      path === "/login" &&
       (req.cookies.get("next-auth.session-token") ||
         req.cookies.get("__Secure-next-auth.session-token"))
     ) {
